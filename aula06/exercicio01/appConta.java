5c863a9c6e4e9ff7f2a3f02229bd71d0b0e6b80f
-v <feaoki@gmail.com>
2020-09-15 23:38:22 -0300

    ultima versao appConta.java feaoki

--- aula06/exercicio01/appConta.java
+++ aula06/exercicio01/appConta.java
@@ -8,9 +8,26 @@ public class appConta {
         
         Scanner teclado = new Scanner(System.in);
         int opcao;
+        System.out.println("App Conta");
 
-        
+        exibirOpcoes();
 
+        do {
+            opcao = Integer.parseInt(teclado.nextLine());
+        } while (opcao == 0);
+
+    }
+
+    public static void exibirOpcoes(){
+        System.out.println("0 - Sair");
+        System.out.println("1 - Criar Conta Normal");
+        System.out.println("2 - Criar Conta Corrente");
+        System.out.println("3 - Criar Conta Poupanca");
+        System.out.println("4 - Criar Conta Especial");
+        System.out.println("5 - Consultar Dados Conta");
+        System.out.println("6 - Depositar");
+        System.out.println("7 - Sacar");
+        System.out.println("8 - Exibir opcoes novamente");
     }
     
 }
