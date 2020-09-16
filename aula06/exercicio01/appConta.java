972dc38c45ca563607903caf60962b06c4638207
fernando eidi aoki <feaoki@gmail.com>
2020-09-15 23:16:47 -0300

    commit do appConta via VSCODE

---
+++ aula06/exercicio01/appConta.java
@@ -0,0 +1,16 @@
+package exercicio01;
+
+import java.util.Scanner;
+
+public class appConta {
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        int opcao;
+
+        
+
+    }
+    
+}
